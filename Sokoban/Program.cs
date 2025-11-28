a35f513c0a08901da2644baca9c97406a4b5e4f8
namwon-RT <namwon1006@gmail.com>
2025-11-28 10:49:10 +0900

    feat: 콘솔 초기화 기능 구현

--- Sokoban/Program.cs
+++ Sokoban/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 초기화
+            Console.ResetColor();        
+            // 색깔 조정
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            // 커서 숨김
+            Console.CursorVisible = false;
+            // 제목 수정
+            Console.Title = "소코반 만들자";
+            //콘솔창 내용 정리
+            Console.Clear();
+            
         }
     }
 }
